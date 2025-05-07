49550dfd4ed15464adbdde7b6308b7c6c1d24ec2
Kamila Sarmanho <kamilasarmanho@gmail.com>
2025-05-07 14:30:15 -0300

    Adicionando códigos
    Dei um commit para colocar o HTML, o JS e a imagem do jogo

---
+++ Minecraft.js
@@ -0,0 +1,5 @@
+function Descricao(texto){
+    document.write('<p>' + texto + '</p>')
+}
+
+Descricao('Minecraft é um jogo 3D que envolve a imaginação: você pode construir, quebrar e colocar novos blocos no seu mundo da maneira que você quiser')
