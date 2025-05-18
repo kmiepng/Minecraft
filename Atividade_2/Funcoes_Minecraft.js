454cec7d0d2bcbc83168731bd7fd47fd62ee08f3
Kamila Sarmanho <kamilasarmanho@gmail.com>
2025-05-18 20:45:59 -0300

    Upgrade Funcoes_Minecraft
    Juntei as classes ferramenta e blocos para apenas 'itens', retirei o drop de xp pois não vou fazer função para encantamento e retirei a função de verificar se existe um pack ou não. Modifiquei a função drop_minerio

--- Atividade_2/Funcoes_Minecraft.js
+++ Atividade_2/Funcoes_Minecraft.js
@@ -1,31 +1,56 @@
-class Ferramenta{
-    constructor(nome, material, durabilidade, velocidade){
-        this.nome = nome; //nome da ferramenta
-        this.material = material; //de que material ela é feita, ex: picareta de ferro
+//classe de itens do mine
+class Itens{
+    constructor(nome, tipo, quantidade = 1, durabilidade = null, fortalecimento = null){
+        this.nome = nome; //nome da ferramenta, ex: picareta de diamante
+        this.tipo = tipo;
+        this.quantidade = quantidade;
         this.durabilidade = durabilidade; //quantos blocos consegue quebrar antes de acabar a ferramenta
-        this.velocidade = velocidade; //velocidade de quebra dos blocos
+        this.fortalecimento = fortalecimento; //aprimoramentos da ferramenta ex: fortuna I
+    }
+    usar_ferramenta(){
+        this.durabilidade > 0? this.durabilidade-- : console.log(`${this.nome} quebrou!`); /* verifica
+        se ainda dá pra usar a ferramenta, se não der é pq tá quebrada */
+    }
+    usar_bloco(){
+        this.quantidade > 0? this.quantidade-- : console.log(`Não há mais ${this.nome} no slot`);
+    }
+    informacao_item(){
+        let informacao = `${this.nome}, Quantidade: ${this.quantidade}`;  //informações básicas
+
+        if (this.durabilidade !== null){
+            if (this.fortalecimento !== null){ //verifica se há algum encantamento para concatenar às infos
+            informacao += `, Durabilidade: ${this.durabilidade}, Fortalecimento: ${this.fortalecimento}`;
+            }
+            informacao += `, Durabilidade: ${this.durabilidade}`;
+        }
+        return console.log(`${informacao}`);
     }
 }
 
-class Bloco{
-    constructor(nome, cores, resistência){
-        this.nome = nome; //nome do bloco
-        this.cores = cores; //esquema de cores simples do bloco
-        this.resistência = resistência; //depende da ferramenta que usa, se ta quebrando com a mao ou não
-        // alguns blocos só quebram com picareta
+//funcoes de algumas funções relacionadas aos itens
+function drop_Minerio(nome_drop, encantamento = null, min_drop = 1, max_drop = 4){  //quantidade de minério que dropa após minerar um bloco de minério
+    //não to considerando os drops extras de carvão, cobre e redstone
+    drop =  Math.floor(Math.random() * (max_drop-min_drop +1)) + min_drop;
+    if (encantamento !== null){ //verificando se quebrou o minério com a picareta encantada
+        encantamento === 'Toque de seda' ? nome_drop = new Itens(`Bloco de ${nome_drop}`, 'Bloco') : nome_drop = new Itens(nome_drop, 'Minério', drop);
     }
-}
-
-let experience_points = Math.random(); // quantidade de xp ganha ao abater monstro
-// usada para aprimorar certas habilidades de uma determinada ferramenta
-
-function verificar_Qtd_blocos(pack){ //função simples para verificar se pode pegar mais um objeto para o pack ou não
-    if (pack.length === 64) {
-        console.log("Não é mais possível adicionar blocos nesse pack, criando novo pack para preencher")
-        let pack_espaco_2 = new Array(64)
+    else{
+        nome_drop = new Itens(nome_drop, 'Minério'); //caso não, dropa apenas 1 minério normal
     }
+    return nome_drop;
 }
 
-function drop_Minerio(min_drop, max_drop){  //quantidade de minério que dropa após minerar um bloco de minério
-    return Math.floor(Math.random() * (max_drop-min_drop +1)) + min_drop;
-}
+//teste
+
+let picareta_de_ferro = new Itens('Picareta de Ferro', 'Ferramenta', 1, '250', 'Toque de seda');
+
+picareta_de_ferro.informacao_item();
+
+picareta_de_ferro.usar_ferramenta();
+let diamante = drop_Minerio('Diamante', 'Fortuna III');
+
+diamante.informacao_item();
+picareta_de_ferro.informacao_item();
+
+let pedregulho = new Itens('Pedregulho', 'Bloco', 64)
+pedregulho.informacao_item();
