259045cf981aec340cf020c1d46fcda62d6c1fa8
Kamila Sarmanho <kamilasarmanho@gmail.com>
2025-05-29 11:47:00 -0300

    Update itens.ts

--- jogo/itens.ts
+++ jogo/itens.ts
@@ -1,3 +1,22 @@
-class Item{
-    
+type tipoItem = 'bloco' | 'ferramenta' | 'armadura' | 'minerio'
+
+interface Item {
+    nome : string
+    quantidade : number
+    tipo : tipoItem
+    encantamento ?: string
+}
+
+export default class Itens {
+    item : Item
+    constructor(item : Item){
+        this.item = item
+    }
+    info(){
+        let informacao = `${this.item.nome}, Quantidade: ${this.item.quantidade}`
+        if (this.item.encantamento !== undefined){
+            informacao += `, Encantamento: ${this.item.encantamento}`
+        }
+        return informacao
+    }
 }
