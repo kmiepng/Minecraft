9cb88aadeada471db953e27ab21cd09f05114c8f
Kamila Sarmanho <kamilasarmanho@gmail.com>
2025-08-22 09:48:34 -0300

    feat: Adição de funções de teste para retornar o desempenho de cada algoritmo

--- jogo/search_e_sort_algoritmos.ts
+++ jogo/search_e_sort_algoritmos.ts
@@ -104,3 +104,19 @@ export function linearSearch(arr : any[], target : any){
             return i;
     return -1;
 }
+//Função teste, com o tempo gasto para processar cada sort incluso
+export function testSort(name: string, sortFunc: (arr: number[]) => number[], data: any[]) {
+    const startTime = performance.now();
+    const sorted = sortFunc(data);
+    const endTime = performance.now();
+
+    return endTime - startTime
+}
+
+export function testSearch(name: string, searchFunc: (arr: any[], target : any) => any, data: any[], target : any) {
+    const startTime = performance.now();
+    const search = searchFunc(data, target);
+    const endTime = performance.now();
+
+    return endTime - startTime
+}
