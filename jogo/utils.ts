824f7109b3f215b714a7984d348f0a916c5dd816
Kamila Sarmanho <kamilasarmanho@gmail.com>
2025-08-27 13:40:58 -0300

    feat: Adição de mais funções no utils.ts

--- jogo/utils.ts
+++ jogo/utils.ts
@@ -171,3 +171,38 @@ export class ValuePair {
         return `[#${this.key}: ${this.value}]\n`;
     }
 }
+export const Compare = {
+    LESS_THAN: -1,
+    BIGGER_THAN: 1,
+    EQUALS: 0
+};
+export const DOES_NOT_EXIST = -1;
+export function lesserEquals(a : any, b : any, compareFn : any) {
+    const comp = compareFn(a, b);
+    return comp === Compare.LESS_THAN || comp === Compare.EQUALS;
+}
+export function biggerEquals(a : any, b : any, compareFn : any) {
+    const comp = compareFn(a, b);
+    return comp === Compare.BIGGER_THAN || comp === Compare.EQUALS;
+}
+export function defaultCompare(a : any, b : any) {
+    if (a === b) {
+    return Compare.EQUALS;
+    }
+    return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
+}
+export function defaultEquals(a : any, b : any) {
+    return a === b;
+}
+export function swap(array : any[], a : any, b : any) {
+    /* const temp = array[a];
+    array[a] = array[b];
+    array[b] = temp; */
+    [array[a], array[b]] = [array[b], array[a]];
+}
+export function reverseCompare(compareFn : any) {
+    return (a : any, b : any) => compareFn(b, a);
+}
+export function defaultDiff(a : any, b : any) {
+    return Number(a) - Number(b);
+}
