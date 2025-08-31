c45e7e78e83e7394d9dcb60ffd56e5593d9c6737
Kamila Sarmanho <kamilasarmanho@gmail.com>
2025-08-31 15:09:30 -0300

    feat: Separação de duas funcionalidades a fim de melhor visualização

---
+++ jogo/lista_ligada_circular.ts
@@ -0,0 +1,70 @@
+//Essa LinkedList duplamente ligada será usada para mostrar o ciclo do dia do jogo
+class Nodee{
+    data : string;
+    prev : Nodee | null;
+    next : Nodee | null;
+    constructor(data : string){
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+    getData(){
+        return this.data;
+    }
+}
+export class ListaLigadaCircularDuasVias {
+    head : Nodee | null;
+    constructor() {
+        this.head = null;
+    }
+    adicionar(horario : string) {
+        const novo = new Nodee(horario);
+        if (!this.head) {
+            novo.next = novo;
+            novo.prev = novo;
+            this.head = novo;
+        } else {
+            const ultimo = this.head.prev;
+            // conecta novo entre o último e o head
+            novo.next = this.head;
+            novo.prev = ultimo;
+
+            this.head.prev = novo;
+            ultimo!.next = novo;
+        }
+    }
+    remover(horario : string) {
+        if (!this.head) return;
+        let atual : (Nodee | null)= this.head;
+        do {
+            if (atual?.data === horario) {
+                if (atual.next === atual) {
+                    // Só um elemento
+                    this.head = null;
+                } else {
+                    atual.prev!.next = atual.next;
+                    atual.next!.prev = atual.prev;
+                    if (atual === this.head) {
+                        this.head = atual.next;
+                    }
+                }
+                return;
+            }
+            atual = atual!.next;
+        } while (atual !== this.head);
+    }
+
+    // Mostra a lista de horarios em ordem
+    print() {
+        if (!this.head) return;
+
+        let info = '| ';
+        let atual : (Nodee | null) = this.head;
+        do {
+            info += (atual?.data + ' | ');
+            atual = atual!.next;
+        } while (atual !== this.head);
+
+        return info;
+    }
+}
--- jogo/main.ts
+++ jogo/main.ts
@@ -2,6 +2,7 @@ import { Itens, ItensPilha } from "./itens";
 import { Inventario, InventarioComPilha } from "./inventario";
 import { bubblesort, mergeSort, linearSearch, binarySearch, testSearch, testSort } from "./search_e_sort_algoritmos";
 import { TrouxaPilha } from "./pilhas";
-import { Jogadores, ListaLigadaCircularDuasVias, Bau } from "./lista_ligada";
+import { ListaLigadaCircularDuasVias } from "./lista_ligada_circular";
+import { Bau, Jogadores } from "./lista_ligada_sets";
 import { Dictionary } from "./dicionario";
 import { HashTable } from "./hashTable";
